2787c32d69a73344ad9b52580ca25eb3ac4da478
Jose <josejesuscedeno@gmail.com>
2022-03-13 23:46:11 +0000

    que bonito

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import json
+
 app = Flask(__name__)
 
 
@@ -8,16 +9,31 @@ todos = [
     { "label": "My second task", "done": True }
 ]
 
+    #Retornando la lista de items:
 @app.route('/todos', methods=['GET'])
 def hello_world():
     return jsonify(todos)
 
+    #creando un item:
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body = request.json
-    print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
- 
+    request_data = request.data
+    decoded_object = json.loads(request_data)
+    print("Incoming request with the following body", request_data)
+    todos.append(decoded_object)
+    return jsonify(todos)
+
+
+    #retornando un item:
+#@app.route('todos/<int:id_item>', methods=['GET'])
+#def get_item(id_item):
+#    item = {}
+#    for element in todos:
+#        if element.get('id') == id_item:
+#            item = element
+#    return jsonify(item)
+
+    #primeros tests:
 #@app.route('/todos', methods=['GET'])
 #def hello_world():
 #    return "<h1>Hello!</h1>"
