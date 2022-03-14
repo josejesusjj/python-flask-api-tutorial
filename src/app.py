e98a283b551bac1119ab671542df762b61820ec8
Jose <josejesuscedeno@gmail.com>
2022-03-14 00:15:32 +0000

    Done

--- src/app.py
+++ src/app.py
@@ -9,12 +9,12 @@ todos = [
     { "label": "My second task", "done": True }
 ]
 
-    #Retornando la lista de items:
+    #Retrieving all items:
 @app.route('/todos', methods=['GET'])
-def hello_world():
+def todo_list():
     return jsonify(todos)
 
-    #creando un item:
+    #creating an item:
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_data = request.data
@@ -23,25 +23,12 @@ def add_new_todo():
     todos.append(decoded_object)
     return jsonify(todos)
 
-    #borrando un item:
+    #erasing an item:
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
     print("This is the position to delete: ",position)
-    return 'something'
-
-    #retornando un item:
-#@app.route('todos/<int:id_item>', methods=['GET'])
-#def get_item(id_item):
-#    item = {}
-#    for element in todos:
-#        if element.get('id') == id_item:
-#            item = element
-#    return jsonify(item)
-
-    #primeros tests:
-#@app.route('/todos', methods=['GET'])
-#def hello_world():
-#    return "<h1>Hello!</h1>"
+    todos.pop(position)
+    return jsonify(todos)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
